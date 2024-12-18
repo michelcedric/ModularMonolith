14d42560b0d0fffaa3006290a4fcff9f64ffd7f4
cedricmichel <cedric.michel@oulook.be>
2024-12-18 10:43:48 +0100

    add missing files

---
+++ src/My.ModularMonolith.MyBModule.Infrastructure/Data/Migrations/20241218092559_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace My.ModularMonolith.MyBModule.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "ModuleB");
+
+            migrationBuilder.CreateTable(
+                name: "ModelBs",
+                schema: "ModuleB",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelBs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModelBs",
+                schema: "ModuleB");
+        }
+    }
+}
